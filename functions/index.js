a8baa657f0e06bf3906dc3b316039e951db0e3b8
bogdano <bogdan.boskovic@outlook.com>
2024-04-22 10:18:07 -0600

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const pug = require("pug");
+
+exports.test = onRequest((request, response) => {
+    let template = pug.compileFile('views/test.pug');
+    let markup = template({
+        name: request.query.name,
+        major: request.query.major,
+        quote: request.query.quote
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(markup);
+});
